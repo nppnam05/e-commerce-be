eae34d6c4685bb33f71b340641bd2fd651a56f84
NamNPP <nppnam05@gmail.com>
2026-06-27 21:04:14 +0700

    update cart

--- src/main/java/com/e_commerce/e_commerce_api/service/OrderService.java
+++ src/main/java/com/e_commerce/e_commerce_api/service/OrderService.java
@@ -15,6 +15,7 @@ import com.e_commerce.e_commerce_api.exception.NotFoundException;
 import com.e_commerce.e_commerce_api.mapper.OrderMapper;
 import com.e_commerce.e_commerce_api.repository.AddressRepository;
 import com.e_commerce.e_commerce_api.repository.CartRepository;
+import com.e_commerce.e_commerce_api.repository.OrderProductRepository;
 import com.e_commerce.e_commerce_api.repository.OrderRepository;
 import com.e_commerce.e_commerce_api.utils.ExceptionGenerator;
 import java.math.BigDecimal;
@@ -29,118 +30,120 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-        private final OrderRepository orderRepository;
-        private final CartRepository cartRepository;
-        private final AddressRepository addressRepository;
-        private final OrderMapper orderMapper;
+    private final OrderRepository orderRepository;
+    private final CartRepository cartRepository;
+    private final AddressRepository addressRepository;
+    private final OrderMapper orderMapper;
+    private final OrderProductRepository orderProductRepository;
 
-        public boolean updateOrderStatus(Long id, String status) {
-                var order = orderRepository.findById(id)
-                                .orElseThrow(() -> new RuntimeException("Order not found"));
-                order.setStatus(status);
-                orderRepository.save(order);
-                return true;
+    public boolean updateOrderStatus(Long id, String status) {
+        var order = orderRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+        order.setStatus(status);
+        orderRepository.save(order);
+        return true;
+    }
+
+    public PageResponse<List<OrderUserResponse>> getOrdersByUserId(Long userId, int pageNumber,
+            int pageSize) {
+        int offset = (pageNumber - 1) * pageSize;
+        var result = orderRepository.findOrdersByUserId(userId, pageSize, offset);
+        var orders = result.stream()
+                .map(p -> (OrderUserResponse) OrderUserResponse.builder().id(p.getId())
+                        .status(p.getStatus()).code(p.getCode()).createdOn(p.getCreatedOn())
+                        .totalPrice(p.getTotalPrice()).totalQuantity(p.getTotalQuantity())
+                        .address("Đường %s, Phường %s, Quận %s, Thành phố %s".formatted(
+                                p.getStreet(), p.getWard(), p.getDistrict(), p.getCity()))
+                        .build())
+                .toList();
+        long total = orderRepository.countOrdersByUserId(userId);
+        return PageResponse.mapToPageResponse(orders, pageNumber, pageSize, total);
+    }
+
+    public PageResponse<List<OrderResponse>> getOrders(LocalDate dateTime, String status,
+            int pageNumber, int pageSize) {
+        int offset = (pageNumber - 1) * pageSize;
+        List<OrderResponse> orders = orderRepository
+                .findAllOrder(dateTime, status, pageSize, offset).stream()
+                .map(p -> OrderResponse.builder().id(p.getId()).status(p.getStatus())
+                        .code(p.getCode()).createdOn(p.getCreatedOn())
+                        .customerName(p.getCustomerName())
+                        .address("Đường %s, Phường %s, Quận %s, Thành phố %s".formatted(
+                                p.getStreet(), p.getWard(), p.getDistrict(), p.getCity()))
+                        .build())
+                .toList();
+        long total = orderRepository.countOrders(dateTime, status);
+        return PageResponse.mapToPageResponse(orders, pageNumber, pageSize, total);
+    }
+
+    public List<MonthlyRevenueResponse> getMonthlyRevenueForThisYear() {
+        var monthlyRevenues = orderRepository.getMonthlyRevenueForThisYear();
+        var result = MonthlyRevenueResponse.initMonths();
+        monthlyRevenues.forEach((value) -> {
+            result.get(value.getMonth() - 1).setRevenue(value.getRevenue());
+        });
+        return result;
+    }
+
+    @Transactional
+    public OrderResponse createOrder(CreateOrderRequest request) {
+        List<Cart> carts = cartRepository.findByUserIdWithDetail(request.getUserId());
+
+        if (carts.isEmpty()) {
+            throw new NotFoundException("User don't have any item on cart");
         }
 
-        public PageResponse<List<OrderUserResponse>> getOrdersByUserId(Long userId, int pageNumber,
-                        int pageSize) {
-                int offset = (pageNumber - 1) * pageSize;
-                var result = orderRepository.findOrdersByUserId(userId, pageSize, offset);
-                var orders = result.stream()
-                                .map(p -> (OrderUserResponse) OrderUserResponse.builder().id(p.getId())
-                                                .status(p.getStatus()).code(p.getCode()).createdOn(p.getCreatedOn())
-                                                .totalPrice(p.getTotalPrice()).totalQuantity(p.getTotalQuantity())
-                                                .address("Đường %s, Phường %s, Quận %s, Thành phố %s".formatted(
-                                                                p.getStreet(), p.getWard(), p.getDistrict(),
-                                                                p.getCity()))
-                                                .build())
-                                .toList();
-                long total = orderRepository.countOrdersByUserId(userId);
-                return PageResponse.mapToPageResponse(orders, pageNumber, pageSize, total);
-        }
+        var totalPrice = carts.stream()
+                .map(cart -> cart.getSinglePrice().multiply(BigDecimal.valueOf(cart.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        public PageResponse<List<OrderResponse>> getOrders(LocalDate dateTime, String status,
-                        int pageNumber, int pageSize) {
-                int offset = (pageNumber - 1) * pageSize;
-                List<OrderResponse> orders = orderRepository
-                                .findAllOrder(dateTime, status, pageSize, offset).stream()
-                                .map(p -> OrderResponse.builder().id(p.getId()).status(p.getStatus())
-                                                .code(p.getCode()).createdOn(p.getCreatedOn())
-                                                .customerName(p.getCustomerName())
-                                                .address("Đường %s, Phường %s, Quận %s, Thành phố %s".formatted(
-                                                                p.getStreet(), p.getWard(), p.getDistrict(),
-                                                                p.getCity()))
-                                                .build())
-                                .toList();
-                long total = orderRepository.countOrders(dateTime, status);
-                return PageResponse.mapToPageResponse(orders, pageNumber, pageSize, total);
-        }
+        var totalQuantity = carts.stream().mapToInt(Cart::getQuantity).sum();
+        var user = carts.stream().findFirst().get().getUser();
+        var address = addressRepository.findById(request.getAddressId()).orElseThrow(
+                () -> ExceptionGenerator.handleNotFoundAddress(request.getAddressId()));
 
-        public List<MonthlyRevenueResponse> getMonthlyRevenueForThisYear() {
-                var monthlyRevenues = orderRepository.getMonthlyRevenueForThisYear();
-                var result = MonthlyRevenueResponse.initMonths();
-                monthlyRevenues.forEach((value) -> {
-                        result.get(value.getMonth() - 1).setRevenue(value.getRevenue());
-                });
-                return result;
-        }
+        String code =
+                "#OR" + UUID.randomUUID().toString().replace("-", "").substring(0, 8).toUpperCase();
 
-        @Transactional
-        public OrderResponse createOrder(CreateOrderRequest request) {
-                List<Cart> carts = cartRepository.findByUserIdWithDetail(request.getUserId());
+        var order = Order.builder().user(user).address(address).code(code)
+                .status(PaymentStatus.PND.name()).paymentStatus(PaymentStatus.PND.name())
+                .totalQuantity(totalQuantity).totalPrice(totalPrice).build();
 
-                if (carts.isEmpty()) {
-                        throw new NotFoundException("User don't have any item on cart");
-                }
+        Order saveOrder = orderRepository.save(order);
 
-                var totalPrice = carts.stream()
-                                .map(cart -> cart.getSinglePrice().multiply(BigDecimal.valueOf(cart.getQuantity())))
-                                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        List<OrderProduct> orderProducts = carts.stream()
+                .map(cart -> OrderProduct.builder().quantity(cart.getQuantity())
+                        .singlePrice(cart.getSinglePrice())
+                        .productChildren(cart.getProductChildren()).order(saveOrder).build())
+                .collect(Collectors.toList());
 
-                var totalQuantity = carts.stream().mapToInt(Cart::getQuantity).sum();
-                var user = carts.stream().findFirst().get().getUser();
-                var address = addressRepository.findById(request.getAddressId()).orElseThrow(
-                                () -> ExceptionGenerator.handleNotFoundAddress(request.getAddressId()));
+        orderProductRepository.saveAll(orderProducts);
 
-                String code = "#OR" + UUID.randomUUID().toString().replace("-", "").substring(0, 8).toUpperCase();
+        saveOrder.setOrderProducts(orderProducts);
 
-                var order = Order.builder().user(user).address(address).code(code)
-                                .status(PaymentStatus.PND.name()).paymentStatus(PaymentStatus.PND.name())
-                                .totalQuantity(totalQuantity).totalPrice(totalPrice).build();
+        cartRepository.deleteAllInBatch(carts);
 
-                Order saveOrder = orderRepository.save(order);
+        return orderMapper.toResponse(saveOrder, address, user);
+    }
 
-                List<OrderProduct> orderProducts = carts.stream()
-                                .map(cart -> OrderProduct.builder().quantity(cart.getQuantity())
-                                                .singlePrice(cart.getSinglePrice()).order(saveOrder)
-                                                .productChildren(cart.getProductChildren()).build())
-                                .collect(Collectors.toList());
+    public OrderDetailResponse getOrderDetail(Long id) {
+        var order = orderRepository.findOrderDetailById(id)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+        var products = orderRepository.findProductsByOrderId(id);
+        List<ProductOrderResponse> productOrderResponses = products.stream()
+                .map(p -> (ProductOrderResponse) ProductOrderResponse.builder().id(p.getId())
+                        .name(p.getName()).price(p.getPrice()).quantity(p.getQuantity())
+                        .category(p.getCategory()).size(p.getSize()).colorCode(p.getColorCode())
+                        .imageUrls(p.getImageUrls() != null ? List.of(p.getImageUrls().split(","))
+                                : List.of())
+                        .build())
+                .toList();
 
-                cartRepository.deleteAll(carts);
-                saveOrder.setOrderProducts(orderProducts);
-
-                return orderMapper.toResponse(saveOrder, address, user);
-        }
-
-        public OrderDetailResponse getOrderDetail(Long id) {
-                var order = orderRepository.findOrderDetailById(id)
-                                .orElseThrow(() -> new RuntimeException("Order not found"));
-                var products = orderRepository.findProductsByOrderId(id);
-                List<ProductOrderResponse> productOrderResponses = products.stream()
-                                .map(p -> (ProductOrderResponse) ProductOrderResponse.builder().id(p.getId())
-                                                .name(p.getName()).price(p.getPrice()).quantity(p.getQuantity())
-                                                .category(p.getCategory()).size(p.getSize()).colorCode(p.getColorCode())
-                                                .imageUrls(p.getImageUrls() != null
-                                                                ? List.of(p.getImageUrls().split(","))
-                                                                : List.of())
-                                                .build())
-                                .toList();
-
-                return OrderDetailResponse.builder().id(order.getId()).code(order.getCode())
-                                .createdOn(order.getCreatedOn()).status(order.getStatus())
-                                .customerName(order.getCustomerName())
-                                .address("Đường %s, Phường %s, Quận %s, Thành phố %s".formatted(order.getStreet(),
-                                                order.getWard(), order.getDistrict(), order.getCity()))
-                                .totalAmount(order.getTotalAmount()).products(productOrderResponses).build();
-        }
+        return OrderDetailResponse.builder().id(order.getId()).code(order.getCode())
+                .createdOn(order.getCreatedOn()).status(order.getStatus())
+                .customerName(order.getCustomerName())
+                .address("Đường %s, Phường %s, Quận %s, Thành phố %s".formatted(order.getStreet(),
+                        order.getWard(), order.getDistrict(), order.getCity()))
+                .totalAmount(order.getTotalAmount()).products(productOrderResponses).build();
+    }
 }
